36f59ce4f39269804c6ebd6279ad7cedd9b94845
nambin <mkkim044@naver.com>
2025-05-26 11:04:19 +0900

    feat : add code

---
+++ index.js
@@ -0,0 +1,4 @@
+export {checkArr} from './src/checkedArr';
+export {findOddNum} from './src/findOddNum';
+export {findEvenNum} from './src/findEvenNum';
+export {capitalize} from './src/capitalize';
